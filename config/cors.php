3b6058394c883a3d2cc19ca81b64e98402ba50a6
justincarter100 <chamounjoseph78@gmail.com>
2026-01-23 10:57:54 +0200

    fixing cors

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://kfarhawrabrotherhood-jc.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
+    
+    'allowed_methods' => ['*'],
+
 
     'exposed_headers' => [],
 
