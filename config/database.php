2dcda273119bc4cbe657c17b1aee9c8a8df6d8d3
justincarter100 <chamounjoseph78@gmail.com>
2026-01-23 09:16:12 +0200

    database fix

--- config/database.php
+++ config/database.php
@@ -63,20 +63,26 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-        'driver' => 'pgsql',
-        'host' => env('DB_HOST', '127.0.0.1'),
-        'port' => env('DB_PORT', '5432'),
-        'database' => env('DB_DATABASE', 'forge'),
-        'username' => env('DB_USERNAME', 'forge'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => 'utf8',
-        'prefix' => '',
-        'prefix_indexes' => true,
-        'schema' => 'public',
-        'sslmode' => 'require',
-        'options' => [],
-    ],
+'pgsql' => [
+    'driver' => 'pgsql',
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'schema' => 'public',
+
+    // 🔑 IMPORTANT
+    'sslmode' => env('DB_SSLMODE', 'require'),
+
+    'options' => extension_loaded('pdo_pgsql') ? array_filter([
+        \PDO::PGSQL_ATTR_SSL_MODE => \PDO::PGSQL_SSL_MODE_REQUIRE,
+    ]) : [],
+],
+
 
 
         'sqlsrv' => [
